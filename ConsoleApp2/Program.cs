394bd6348d4bbb22a9191030ec934d3ede037d54
Michael Bender <mikethemerchant@gmail.com>
2019-07-23 15:51:28 -0400

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+
+namespace SmileOutPut
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var count = CountSmileys(new string[] { ":D", ":~)", ";~D", ":)" });
+            Console.WriteLine($"number of smiles: {count}");
+            count = CountSmileys(new string[] { ":)", ":(", ":D", ":O", ":;" });
+            Console.WriteLine($"number of smiles: {count}");
+            count = CountSmileys(new string[] { ";]", ":[", ";*", ":$", ";-D" });
+            Console.WriteLine($"number of smiles: {count}");
+            count = CountSmileys(new string[] { ";", ")", ";*", ":$", "8-D" });
+            Console.WriteLine($"number of smiles: {count}");
+        }
+
+
+        public static int CountSmileys(string[] smileys)
+        {
+            int count = 0;
+            foreach (var smile in smileys)
+            {
+                //Console.WriteLine(smile);
+                if (smile.Contains(':') || smile.Contains(';'))
+                {
+                    if (smile.Contains(')') || smile.Contains('D'))
+                    {
+                        //Console.WriteLine(smile.Length);
+                        if (smile.Length == 2)
+                        {
+                            count++;
+                            //Console.WriteLine("counted");
+                        }
+                        else
+                        {
+                            if (smile.Contains('~') || smile.Contains('-'))
+                            {
+                                count++;
+                                //Console.WriteLine("counted");
+                            }
+                        }
+
+                    }
+                }
+            }
+
+            return count;
+        }
+    }
+}
